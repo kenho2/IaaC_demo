fc70eb824fc2c6f42a82e9b94900a0d4067a344c
kenho2 <ken.ho.mail@gmail.com>
2026-06-13 19:29:31 -0700

    first code review done

--- modules/proxmox_lxc/outputs.tf
+++ modules/proxmox_lxc/outputs.tf
@@ -1,19 +1,4 @@
-output "id" {
-  description = "Terraform resource identifier"
-  value       = proxmox_virtual_environment_container.this.id
-}
+The user wants me to apply a suggested edit to an original piece of Terraform output code.
 
-output "vm_id" {
-  description = "Proxmox container ID"
-  value       = proxmox_virtual_environment_container.this.vm_id
-}
+Original Code:
 
-output "ipv4" {
-  description = "IPv4 address map per network interface"
-  value       = proxmox_virtual_environment_container.this.ipv4
-}
-
-output "ipv6" {
-  description = "IPv6 address map per network interface"
-  value       = proxmox_virtual_environment_container.this.ipv6
-}
